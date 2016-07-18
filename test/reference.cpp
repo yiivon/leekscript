649f7db079282b3a624251f17a6a6599e2da2464
Pilow <pierre.laupretre@gmail.com>
2016-07-18 19:20:29 +0200

    Fix references

--- test/reference.cpp
+++ test/reference.cpp
@@ -3,13 +3,13 @@
 void Test::test_references() {
 
 	header("References");
-/*
+
 	success("let a = 2 let b = a a = 4 b", "2");
 	success("let a = 2 let b = @a a = 5 b", "5");
 	success("let a = 2 let b = @a b = 6 a", "6");
 	success("let a = 2 let b = @a let c = @b a = 7 c", "7");
 	success("let a = 2 let b = @a let c = @b c = 8 a", "8");
-
+/*
 	success("let a = 'hello' let b = @a a = 'world' b", "'world'");
 	success("let a = true let b = @a b = false b", "false");
 	success("let a = true let b = @a b = false a", "false");
