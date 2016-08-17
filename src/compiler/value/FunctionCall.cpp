418ef0dc94a50fcc71a04e6acfea6a3180ca1a04
Mario Geiger <geiger.mario@gmail.com>
2016-08-17 22:20:33 +0200

    ...

--- src/compiler/value/FunctionCall.cpp
+++ src/compiler/value/FunctionCall.cpp
@@ -513,7 +513,7 @@ jit_value_t FunctionCall::compile(Compiler& c) const {
 
 		// Destroy temporary arguments
 		for (int i = 0; i < arg_count - 1; ++i) {
-			if (function->type.getArgumentType(i).nature == Nature::POINTER) {
+			if (function->type.getArgumentType(i).must_manage_memory()) {
 				VM::delete_temporary(c.F, args[i + 1]);
 			}
 		}
