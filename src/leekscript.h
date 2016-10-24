a8ffc83791991b290680d9370fdf917af4439982
Pilow <pierre.laupretre@gmail.com>
2016-10-24 21:38:41 +0200

    Add a comment about types in header

--- src/leekscript.h
+++ src/leekscript.h
@@ -1,6 +1,27 @@
 #ifndef LEEKSCRIPT_H__
 #define LEEKSCRIPT_H__
 
+/*
+value
+	 ---> null
+	 ---> boolean
+	 ---> number
+	 			---> real
+						 ---> long
+						 		  ---> integer
+	 ---> map<K, V>
+	 		 ---> object = map<string, ?>
+
+	 	     ---> array<V> = map<int, V>
+			   		   ---> string = array<int> (= map<int, int>)
+					   ---> interval = array<int> (= map<int, int>)
+
+		     ---> set<V> = map<V, null>
+
+	 ---> function
+	 ---> class
+*/
+
 #include "vm/VM.hpp"
 #include "vm/Module.hpp"
 #include "vm/value/LSNumber.hpp"
