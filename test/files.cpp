341e72915fca8003ca9b22b2048a52bd4ae06856
Pilow <pierre.laupretre@gmail.com>
2016-10-24 21:46:04 +0200

    [test] Add reachable code in tests

--- test/files.cpp
+++ test/files.cpp
@@ -4,12 +4,9 @@ void Test::test_files() {
 
 	/** Complex codes */
 	header("Files");
-
 	file("test/code/primes.leek").equals("78498");
 	file("test/code/gcd.leek").equals("151");
 	file("test/code/strings.leek").almost(52.0, 2.0);
 	file("test/code/project_euler_62.leek").equals("127035954683");
-
-
-	//test_file("test/code/array.ls", "1365750");
+	file("test/code/reachable_cells.leek").equals("383");
 }
