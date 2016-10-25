43c8cddc83191ab2d5d025c5dd61548fadcf009a
Pilow <pierre.laupretre@gmail.com>
2016-10-25 21:31:16 +0200

    [test][object] Add test for object.map()

--- src/vm/standard/ObjectSTD.cpp
+++ src/vm/standard/ObjectSTD.cpp
@@ -4,7 +4,7 @@
 
 namespace ls {
 
-LSObject* object_map(const LSObject* object);
+//LSObject* object_map(const LSObject* object);
 
 ObjectSTD::ObjectSTD() : Module("Object") {
 
@@ -12,17 +12,18 @@ ObjectSTD::ObjectSTD() : Module("Object") {
 		{Type::OBJECT, Type::POINTER, Type::BOOLEAN, (void*) &LSObject::in}
 	});
 
-	method("map", Type::OBJECT, Type::OBJECT, {}, (void*) &object_map);
+	//method("map", Type::OBJECT, Type::OBJECT, {}, (void*) &object_map);
 
 	method("keys", Type::OBJECT, Type::STRING_ARRAY, {}, (void*) &LSObject::ls_get_keys);
 	method("values", Type::OBJECT, Type::PTR_ARRAY, {}, (void*) &LSObject::ls_get_values);
 }
 
+/*
 LSObject* object_map(const LSObject*) {
 
 	LSObject* new_obj = new LSObject();
 
 	return new_obj;
 }
-
+*/
 }
--- test/object.cpp
+++ test/object.cpp
@@ -43,4 +43,7 @@ void Test::test_objects() {
 
 	code("'x' in {x: 5, y: 'yo'}.keys()").equals("true");
 	code("'x' in {a: 5, y: 'yo'}.keys()").equals("false");
+
+	section("map()");
+	//code("{x: 12, y: 5}.map(x -> x + 1)", "{}");
 }
