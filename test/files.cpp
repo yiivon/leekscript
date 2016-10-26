806140934715c5c205f1cb2c15ee7390a93cf86d
Pilow <pierre.laupretre@gmail.com>
2016-10-26 21:10:29 +0200

    [test] Add Project Euler codes 1 to 6

--- test/files.cpp
+++ test/files.cpp
@@ -9,4 +9,12 @@ void Test::test_files() {
 	file("test/code/strings.leek").almost(52.0, 2.0);
 	file("test/code/project_euler_62.leek").equals("127035954683");
 	file("test/code/reachable_cells.leek").equals("383");
+
+	file("test/code/euler/pe001.leek").equals("233168");
+	file("test/code/euler/pe002.leek").equals("4613732");
+	file("test/code/euler/pe003.leek").equals("6857");
+	file("test/code/euler/pe004.leek").equals("906609");
+	file("test/code/euler/pe005.leek").equals("232792560");
+	file("test/code/euler/pe006.leek").equals("25164150");
+	file("test/code/euler/pe062.leek").equals("127035954683");
 }
