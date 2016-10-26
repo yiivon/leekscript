4818fd2e2967d26cced4ebf742dcdeb431054a1d
Pilow <pierre.laupretre@gmail.com>
2016-10-26 21:10:55 +0200

    [test] Reduce execution time for some test codes

--- test/files.cpp
+++ test/files.cpp
@@ -4,10 +4,9 @@ void Test::test_files() {
 
 	/** Complex codes */
 	header("Files");
-	file("test/code/primes.leek").equals("78498");
+	file("test/code/primes.leek").equals("41538");
 	file("test/code/gcd.leek").equals("151");
-	file("test/code/strings.leek").almost(52.0, 2.0);
-	file("test/code/project_euler_62.leek").equals("127035954683");
+	file("test/code/strings.leek").almost(52.0, 3.0);
 	file("test/code/reachable_cells.leek").equals("383");
 
 	file("test/code/euler/pe001.leek").equals("233168");
