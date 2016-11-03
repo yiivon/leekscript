f34a61f93c25b1752247e0e3d3dfa69f79238d7f
Pilow <pierre.laupretre@gmail.com>
2016-11-03 21:40:59 +0100

    [test] Change section title

--- test/object.cpp
+++ test/object.cpp
@@ -44,6 +44,6 @@ void Test::test_objects() {
 	code("'x' in {x: 5, y: 'yo'}.keys()").equals("true");
 	code("'x' in {a: 5, y: 'yo'}.keys()").equals("false");
 
-	section("map()");
+	//section("Object.map()");
 	//code("{x: 12, y: 5}.map(x -> x + 1)", "{}");
 }
