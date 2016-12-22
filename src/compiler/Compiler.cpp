e4c685a95649ad921560462f2a5b77432b4bf22d
Pilow <pierre.laupretre@gmail.com>
2016-12-22 21:27:08 +0100

    [compiler] Fix to_bool method with functions and strings

--- src/compiler/Compiler.cpp
+++ src/compiler/Compiler.cpp
@@ -172,14 +172,14 @@ Compiler::value Compiler::insn_to_bool(Compiler::value v) const {
 	if (v.t == Type::INTEGER) {
 		return {jit_insn_to_bool(F, v.v), Type::BOOLEAN};
 	}
-	if (v.t == Type::STRING) {
+	if (v.t.raw_type == RawType::STRING) {
 		return insn_call(Type::BOOLEAN, {v}, (void*) &LSString::isTrue);
 	}
 	if (v.t.raw_type == RawType::ARRAY) {
 		// Always take LSArray<int>, but the array is not necessarily of this type
 		return insn_call(Type::BOOLEAN, {v}, (void*) &LSArray<int>::isTrue);
 	}
-	if (v.t == Type::FUNCTION_P) {
+	if (v.t.raw_type == RawType::FUNCTION) {
 		return new_bool(true);
 	}
 	// TODO other types
