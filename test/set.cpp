c27e2459c8ffea1c4419155f832056b72e5196f0
Pilow <pierre.laupretre@gmail.com>
2017-01-09 21:06:14 +0100

    [set][test] Reorganize set tests

--- test/set.cpp
+++ test/set.cpp
@@ -9,17 +9,22 @@ void Test::test_set() {
 	code("<1.56, -2.312, 7.23>").equals("<-2.312, 1.56, 7.23>");
 	code("<'2', '1', '1'>").equals("<'1', '2'>");
 
-	code("let s = <1, 2> s.insert(3) s").equals("<1, 2, 3>");
-	code("let s = <1, 2> s.clear() s").equals("<>");
-	code("let s = <1, 2> s.erase(3)").equals("false");
-	code("let s = <1, 2> s.erase(1)").equals("true");
-	code("let s = <1, 2> s.contains(3)").equals("false");
-	code("let s = <1, 2> s.contains(1)").equals("true");
+	section("Set.operator in ()");
 	code("let s = <1, 2> 3 in s").equals("false");
 	code("let s = <1, 2> 1 in s").equals("true");
+	section("Set.contains()");
+	code("let s = <1, 2> s.contains(3)").equals("false");
+	code("let s = <1, 2> s.contains(1)").equals("true");
 
-	// Type changes
-	// code("let s = <1, 2> s += 'a' s").equals("<1, 2, 'a'>");
+	section("Set.clear()");
+	code("let s = <1, 2> s.clear() s").equals("<>");
+
+	section("Set.erase()");
+	code("let s = <1, 2> s.erase(3)").equals("false");
+	code("let s = <1, 2> s.erase(1)").equals("true");
+
+	section("Set.insert()");
+	code("let s = <1, 2> s.insert(3) s").equals("<1, 2, 3>");
 
 	section("Set.size()");
 	code("<>.size()").equals("0");
@@ -32,4 +37,7 @@ void Test::test_set() {
 
 	section("Set clone()");
 	code("let s = <1, 2, 3> [s]").equals("[<1, 2, 3>]");
+
+	// Type changes
+	// code("let s = <1, 2> s += 'a' s").equals("<1, 2, 'a'>");
 }
