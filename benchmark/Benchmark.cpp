3ad4a65226ad88865c9b1845ba86a3369579e4b0
Pilow <pierre.laupretre@gmail.com>
2017-01-24 23:47:41 +0100

    [benchmark] Start real automated benchmark, with c++, java, ls and python

--- benchmark/Benchmark.cpp
+++ benchmark/Benchmark.cpp
@@ -8,21 +8,137 @@
 #include <iterator>
 #include <string>
 #include <chrono>
+#include <sstream>
+#include <functional>
+#include <iomanip>
 using namespace std;
 
-//#define
+std::string pad(std::string s, int l) {
+	l -= s.size();
+	while (l-- > 0) s += " ";
+	return s;
+}
 
-Benchmark::Benchmark() {}
-Benchmark::~Benchmark() {}
+long chronotime(std::function<void()> f) {
+	auto start = chrono::high_resolution_clock::now();
+	f();
+	auto end = chrono::high_resolution_clock::now();
+	return chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+}
 
-bool is_prime_fast(int number) {
-
-	for (int k = 1; 36 * k * k - 12 * k < number; ++k) {
-		if ((number % (6 * k + 1) == 0) or (number % (6 * k - 1) == 0)) {
-			return false;
-		}
+std::string format_ns(long ns, long reference = 0) {
+	long micro = ns / 1000;
+	long ms = micro / 1000;
+	std::ostringstream oss;
+	oss << ms << " ms";
+	if (reference > 0) {
+		oss << " x" << std::setprecision(3) << ((double) ns / reference);
 	}
-	return true;
+	return oss.str();
+}
+
+/*
+ * Compile and execute the C++ version of a benchmark case
+ */
+long cpp(const std::string& code) {
+	std::ostringstream compile;
+	// .exe to clean binary files more easily
+	compile << "g++ -O2 -std=c++17 benchmark/code/" << code << "/" << code << ".cpp -o benchmark/code/" << code << "/" << code << ".exe";
+	system(compile.str().c_str());
+
+	std::ostringstream execute;
+	execute << "benchmark/code/" << code << "/" << code << ".exe >> results";
+	long time = chronotime([&]() {
+		system(execute.str().c_str());
+	});
+
+	std::ostringstream clean;
+	clean << "rm benchmark/code/" << code << "/" << code << ".exe";
+	system(clean.str().c_str());
+
+	return time;
+}
+
+/*
+ * Compile and execute the Java version of a benchmark case
+ */
+long java(const std::string& code) {
+	std::ostringstream compile;
+	// .exe to clean binary files more easily
+	compile << "javac benchmark/code/" << code << "/" << code << ".java";
+	system(compile.str().c_str());
+
+	std::ostringstream execute;
+	execute << "cd benchmark/code/" << code << " && java " << code << " >> ../../../results";
+	long time = chronotime([&]() {
+		system(execute.str().c_str());
+	});
+
+	std::ostringstream clean;
+	clean << "rm benchmark/code/" << code << "/" << code << ".class";
+	system(clean.str().c_str());
+
+	return time;
+}
+
+/*
+ * Execute the leekscript version of a benchmark case
+ */
+long leekscript(const std::string& code) {
+	std::ostringstream execute;
+	execute << "build/leekscript benchmark/code/" << code << "/" << code << ".leek >> results";
+	return chronotime([&]() {
+		system(execute.str().c_str());
+	});
+}
+
+/*
+ * Execute the Python version of a benchmark case
+ */
+long python(const std::string& code) {
+	std::ostringstream execute;
+	execute << "python benchmark/code/" << code << "/" << code << ".py >> results";
+	return chronotime([&]() {
+		system(execute.str().c_str());
+	}) * 100; // Python scripts do 100x less work, to have a faster benchmark
+}
+
+void benchmark(const std::string& code) {
+	system((string("echo \"") + code + "\" >> results").c_str());
+	std::cout << "│ " << pad(code, 22) << "│ " << std::flush;
+
+	long t = cpp(code);
+	long ref = t;
+	std::cout << pad(format_ns(t, 0), 15) << "│ " << std::flush;
+
+	t = java(code);
+	std::cout << pad(format_ns(t, ref), 15) << "│ " << std::flush;
+
+	t = leekscript(code);
+	std::cout << pad(format_ns(t, ref), 15) << "│ " << std::flush;
+
+	t = python(code);
+	std::cout << "~" << pad(format_ns(t, ref), 14);
+
+	std::cout << "│" << std::endl;
+	system("echo \"\" >> results");
+}
+
+int main(int, char**) {
+	cout << "Starting benchmark..." << endl;
+	std::remove("results");
+
+	cout << "┌───────────────────────┬────────────────┬────────────────┬────────────────┬────────────────┐" << endl;
+	cout << "│ Test case             │ C++ -O2        │ Java           │ LeekScript     │ Python         │" << endl;
+	cout << "├───────────────────────┼────────────────┼────────────────┼────────────────┼────────────────┤" << endl;
+
+	long t = chronotime([&]() {
+	});
+
+	cout << "└───────────────────────┴────────────────┴────────────────┴────────────────┴────────────────┘" << endl;
+
+	cout << " Total time: " << format_ns(t, 0) << endl;
+	cout << endl;
 }
 
 void Benchmark::arrays() {
@@ -49,16 +165,9 @@ void Benchmark::primes() {
 
 	auto exe_start = chrono::high_resolution_clock::now();
 
-	int c = 2;
-	for (int j = 5; j < 1000000; j += 6) {
-		if (is_prime_fast(j)) c++;
-		if (is_prime_fast(j + 2)) c++;
-	}
-	cout << c << endl;
 
 	auto exe_end = chrono::high_resolution_clock::now();
 	long exe_time_ns = chrono::duration_cast<chrono::nanoseconds>(exe_end - exe_start).count();
 	double exe_time_ms = (((double) exe_time_ns / 1000) / 1000);
 	cout << exe_time_ms << endl;
 }
-
