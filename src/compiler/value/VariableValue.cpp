99dc01c344e82f6064f592e01d5db4b00066c6da
Pilow <pierre.laupretre@gmail.com>
2017-01-26 23:48:19 +0100

    [variable] Remove useless internal vars from VariableValue compile_l

--- src/compiler/value/VariableValue.cpp
+++ src/compiler/value/VariableValue.cpp
@@ -144,20 +144,12 @@ Compiler::value VariableValue::compile_l(Compiler& c) const {
 	}
 
 	jit_value_t v;
-
-	if (scope == VarScope::INTERNAL) {
-
-		v = internals[name];
-
-	} else if (scope == VarScope::LOCAL) {
-
+	// No internal values here
+	if (scope == VarScope::LOCAL) {
 		v = c.get_var(name).value;
-
 	} else { /* if (scope == VarScope::PARAMETER) */
-
 		v = jit_value_get_param(c.F, 1 + var->index); // 1 offset for function ptr
 	}
-
 	return c.insn_address_of({v, type});
 }
 
