69dff3979136485b324ed1fb757fc03194080d90
Pilow <pierre.laupretre@gmail.com>
2017-02-17 22:40:50 +0100

    [object][test] Add test with object method access

--- test/object.cpp
+++ test/object.cpp
@@ -50,6 +50,7 @@ void Test::test_objects() {
 	code("let o = {} o.new_val = 12 o").equals("{new_val: 12}");
 	code("let o = {a: 'a'} o.b = 'b' o").equals("{a: 'a', b: 'b'}");
 	code("Object.readonly.v = 5").exception(ls::VM::Exception::CANT_MODIFY_READONLY_OBJECT);
+	code("let o = [{}, ''][0] o.values").equals("<function>");
 
 	/*
 	 * Methods
