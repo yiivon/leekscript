dd82a76671da1ae7eb09a7a8f3858dcce8cf8eba
Pilow <pierre.laupretre@gmail.com>
2017-03-06 21:11:27 +0100

    [string] Remove useless tests and re-enable others

--- test/string.cpp
+++ test/string.cpp
@@ -118,9 +118,9 @@ void Test::test_strings() {
 	section("String iteration");
 	code("for c in 'bonjour' { System.print(c) }").output("b\no\nn\nj\no\nu\nr\n");
 	code("for (c in '汉堡 漢堡') { System.print(c) }").output("汉\n堡\n \n漢\n堡\n");
-	// code("[for c in 'salut' { c }]").equals("['s', 'a', 'l', 'u', 't']");
-	// code("[for c in 'salut' { (c.code() + 2).char() }]").equals("['u', 'c', 'n', 'w', 'v']");
-	// code("let a = [for c in 'salut' { (c.code() + 2).char() }] a.join('')").equals("'ucnwv'");
+	code("[for c in 'salut' { c }]").equals("['s', 'a', 'l', 'u', 't']");
+	code("[for c in 'salut' { (c.code() + 2).char() }]").equals("['u', 'c', 'n', 'w', 'v']");
+	code("let a = [for c in 'salut' { (c.code() + 2).char() }] a.join('')").equals("'ucnwv'");
 	code("var r = '' for k : c in 'azerty' { r += k + '_' + c + '_' } r").equals("'0_a_1_z_2_e_3_r_4_t_5_y_'");
 
 	/*
@@ -140,12 +140,10 @@ void Test::test_strings() {
 	section("String.toUpper()");
 	code("String.toUpper('')").equals("''");
 	code("String.toUpper('salut')").equals("'SALUT'");
-	//code("String.toUpper('ça va ?')").equals("'Ça va ?'");
 
 	section("String.toLower()");
 	code("String.toLower('')").equals("''");
 	code("String.toLower('SALUT')").equals("'salut'");
-	//code("String.toLower('ÇÀÉÈÔÖÛ')").equals("'çaéèôöû'");
 
 	code("String.length('salut')").equals("5");
 	code("String.reverse('salut')").equals("'tulas'");
