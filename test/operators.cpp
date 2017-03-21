ac90fd38454ff9379834e1b65bd27b09486520ee
Pilow <pierre.laupretre@gmail.com>
2017-03-21 22:25:53 +0100

    [test] Add NO_SUCH_OPERATOR tests with array access

--- test/operators.cpp
+++ test/operators.cpp
@@ -62,6 +62,8 @@ void Test::test_operators() {
 	code("let a = null !a").equals("true");
 	code("|null|").exception(ls::VM::Exception::NO_SUCH_OPERATOR);
 	code("null[2] = 5").semantic_error(ls::SemanticError::Type::VALUE_MUST_BE_A_CONTAINER, {"null"});
+	code("let a = [null, ''][0] a[2]").exception(ls::VM::Exception::NO_SUCH_OPERATOR);
+	code("let a = [null, ''][0] a[2] = 5").exception(ls::VM::Exception::NO_SUCH_OPERATOR);
 	code("null[2:5]").semantic_error(ls::SemanticError::Type::VALUE_MUST_BE_A_CONTAINER, {"null"});
 	code("null[2:5] = 4").semantic_error(ls::SemanticError::Type::VALUE_MUST_BE_A_CONTAINER, {"null"});
 	code("(5 + 2) += 4").semantic_error(ls::SemanticError::Type::VALUE_MUST_BE_A_LVALUE, {"5 + 2"});
