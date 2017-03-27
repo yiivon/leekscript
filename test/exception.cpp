7b333a4c0762510648a65bf51c2dbb252d1cff50
Pilow <pierre.laupretre@gmail.com>
2017-03-27 23:30:45 +0200

    [exception][test] Add tests with exceptions and leak risks

--- test/exception.cpp
+++ test/exception.cpp
@@ -10,4 +10,10 @@ void Test::test_exceptions() {
 
 	code("var a = 12m; throw").exception(ls::VM::Exception::EXCEPTION);
 	code("var a = 12m + 5m; throw").exception(ls::VM::Exception::EXCEPTION);
+
+	section("Exceptions - Avoid leaks");
+	code("var x = 'hello' [][0]").exception(ls::VM::Exception::ARRAY_OUT_OF_BOUNDS);
+	code("var x = 'hello' throw").exception(ls::VM::Exception::EXCEPTION);
+	code("let f = { var x = 'hello' [][0] } f()").exception(ls::VM::Exception::ARRAY_OUT_OF_BOUNDS);
+	code("let f = -> { var x = 'hello' throw } f()").exception(ls::VM::Exception::EXCEPTION);
 }
