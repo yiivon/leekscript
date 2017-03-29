73dae3736152b9df651bbb2566ca241a754be81f
Pilow <pierre.laupretre@gmail.com>
2017-03-29 20:45:02 +0200

    [object-access] Remove useless parameter

--- src/compiler/value/ObjectAccess.cpp
+++ src/compiler/value/ObjectAccess.cpp
@@ -122,7 +122,7 @@ void ObjectAccess::analyse(SemanticAnalyser* analyser, const Type& req_type) {
 	}
 }
 
-void ObjectAccess::change_type(SemanticAnalyser*, const Type& req_type) {}
+void ObjectAccess::change_type(SemanticAnalyser*, const Type&) {}
 
 Compiler::value ObjectAccess::compile(Compiler& c) const {
 
