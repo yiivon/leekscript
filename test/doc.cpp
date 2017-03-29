d76267747bc64e1e86b1f4fc1eaaa0ce86f5418d
Pilow <pierre.laupretre@gmail.com>
2017-03-29 21:07:35 +0200

    [doc] Add a test with Documentation destructor

--- test/doc.cpp
+++ test/doc.cpp
@@ -10,4 +10,7 @@ void Test::test_doc() {
 	std::cout << oss.str().substr(0, 300) << " [...]" << std::endl;
 
 	doc.generate(oss, "wrong_lang");
+
+	auto doc2 = new ls::Documentation();
+	delete doc2;
 }
