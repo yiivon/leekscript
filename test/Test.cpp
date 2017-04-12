5f7ec148c1ac666f1c4b8e211901c5532d546b8f
Pilow <pierre.laupretre@gmail.com>
2017-04-12 23:46:23 +0200

    [test] Fix Test::Input::exception condition in case of NO_EXCEPTION

--- test/Test.cpp
+++ test/Test.cpp
@@ -326,8 +326,9 @@ void Test::Input::exception(ls::VM::Exception expected, std::vector<ls::VM::exce
 	auto result = run(false);
 
 	auto actual_type = result.exception != nullptr ? result.exception->type : ls::VM::Exception::NO_EXCEPTION;
+	auto actual_frames = result.exception != nullptr ? result.exception->frames : std::vector<ls::VM::exception_frame>();
 
-	if (actual_type == expected /* && result.exception->frames == frames */) {
+	if (actual_type == expected && (actual_frames == frames || expected == ls::VM::Exception::NO_EXCEPTION)) {
 		pass(result.exception != nullptr ? result.exception->to_string() : "(no exception)");
 	} else {
 		ls::VM::ExceptionObj expected_exception(expected);
