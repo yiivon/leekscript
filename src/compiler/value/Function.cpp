b2a866989e8abbf2284551488e35f227fdeb1a85
Pilow <pierre.laupretre@gmail.com>
2017-05-24 23:02:04 +0200

    [function] Fix print method

--- src/compiler/value/Function.cpp
+++ src/compiler/value/Function.cpp
@@ -94,8 +94,8 @@ void Function::print_version(std::ostream& os, int indent, bool debug, const Ver
 }
 
 void Function::print(std::ostream& os, int indent, bool debug) const {
-	if (has_version) {
-		print_version(os, indent, debug, versions.at(version));
+	if (has_version || versions.size() == 1) {
+		print_version(os, indent, debug, versions.begin()->second);
 	} else {
 		print_version(os, indent, debug, default_version);
 	}
