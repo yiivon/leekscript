32e91f2ac8119fdea1f0e614f1edaa07dd35318e
Pilow <pierre.laupretre@gmail.com>
2017-06-06 23:56:59 +0200

    [variable-value] Fix set_version

--- src/compiler/value/VariableValue.cpp
+++ src/compiler/value/VariableValue.cpp
@@ -65,7 +65,6 @@ bool VariableValue::will_take(SemanticAnalyser* analyser, const vector<Type>& ar
 		var->value->will_take(analyser, args, level);
 		if (auto f = dynamic_cast<Function*>(var->value)) {
 			if (f->versions.find(args) != f->versions.end()) {
-				// std::cout << "[vv] set type from version " << args << std::endl;
 				var->type = f->versions.at(args)->type;
 				var->version = args;
 				var->has_version = true;
@@ -78,16 +77,15 @@ bool VariableValue::will_take(SemanticAnalyser* analyser, const vector<Type>& ar
 		type = var->type;
 		types = type;
 	}
-	// set_version(args);
+	set_version(args, level);
 	return false;
 }
 
 void VariableValue::set_version(const vector<Type>& args, int level) {
-	if (level > 1) {
-		if (var != nullptr and var->value != nullptr) {
-			var->value->set_version(args, level);
-		}
-	} else {
+	if (var != nullptr and var->value != nullptr) {
+		var->value->set_version(args, level);
+	}
+	if (level == 1) {
 		version = args;
 		has_version = true;
 	}
