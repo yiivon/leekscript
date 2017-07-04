f70b18910be229909de6ab41b112009a9bff810e
Pilow <pierre.laupretre@gmail.com>
2017-07-04 22:05:31 +0200

    [class-declaration] Add class field to the constructed LSClass

--- src/compiler/instruction/ClassDeclaration.cpp
+++ src/compiler/instruction/ClassDeclaration.cpp
@@ -39,12 +39,29 @@ void ClassDeclaration::analyse(SemanticAnalyser* analyser, const Type&) {
 
 	for (auto vd : fields) {
 		vd->analyse(analyser, Type::UNKNOWN);
+		for (size_t i = 0; i < vd->variables.size(); ++i) {
+			// std::cout << "Add class field '" << vd->variables.at(i)->content << "' type " << vd->expressions.at(i)->type << std::endl;
+			auto t = i < vd->expressions.size() ? vd->expressions.at(i)->type : Type::NULLL;
+			ls_class->addField(vd->variables.at(i)->content, t, nullptr);
+		}
 	}
 }
 
 Compiler::value ClassDeclaration::compile(Compiler& c) const {
 
 	auto clazz = c.new_pointer(ls_class);
+
+	for (auto vd : fields) {
+		for (size_t i = 0; i < vd->variables.size(); ++i) {
+			// std::cout << "Compile class field '" << vd->variables.at(i)->content << "' type " << vd->expressions.at(i)->type << std::endl;
+			auto default_value = vd->expressions.at(i)->compile(c);
+			default_value = c.insn_to_pointer(default_value);
+			auto field_name = c.new_pointer(&vd->variables.at(i)->content);
+			c.insn_call(Type::VOID, {clazz, field_name, default_value}, +[](LSClass* clazz, std::string* field_name, LSValue* default_value) {
+				clazz->fields.at(*field_name).default_value = default_value;
+			});
+		}
+	}
 
 	c.add_var(name, clazz.v, Type::CLASS, true);
 
