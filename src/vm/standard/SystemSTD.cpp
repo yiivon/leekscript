ca59edb1e711be102b43af7264c67c9e9545dce4
Pilow <pierre.laupretre@gmail.com>
2017-07-09 22:38:12 +0200

    [system][std] Use static_field with native functions for time functions

--- src/vm/standard/SystemSTD.cpp
+++ src/vm/standard/SystemSTD.cpp
@@ -9,10 +9,10 @@ namespace ls {
 
 Compiler::value System_operations(Compiler& c);
 Compiler::value System_version(Compiler& c);
-Compiler::value System_time(Compiler& c);
-Compiler::value System_millitime(Compiler& c);
-Compiler::value System_microtime(Compiler& c);
-Compiler::value System_nanotime(Compiler& c);
+long System_time();
+long System_millitime();
+long System_microtime();
+long System_nanotime();
 void System_print(LSValue* v);
 void System_print_int(int v);
 void System_print_mpz(__mpz_struct v);
@@ -25,10 +25,10 @@ SystemSTD::SystemSTD() : Module("System") {
 
 	static_field("version", Type::INTEGER, System_version);
 	static_field("operations", Type::INTEGER, System_operations);
-	static_field("time", Type::LONG, System_time);
-	static_field("milliTime", Type::LONG, System_millitime);
-	static_field("microTime", Type::LONG, System_microtime);
-	static_field("nanoTime", Type::LONG, System_nanotime);
+	static_field("time", Type::LONG, (void*) &System_time);
+	static_field("milliTime", Type::LONG, (void*) &System_millitime);
+	static_field("microTime", Type::LONG, (void*) &System_microtime);
+	static_field("nanoTime", Type::LONG, (void*) &System_nanotime);
 
 	method("print", Method::Static, {
 		{Type::VOID, {Type::POINTER}, (void*) &System_print, Method::NATIVE},
@@ -41,25 +41,25 @@ SystemSTD::SystemSTD() : Module("System") {
 	});
 }
 
-long get_sec_time() {
+long System_time() {
 	return std::chrono::duration_cast<std::chrono::seconds>(
 		std::chrono::system_clock::now().time_since_epoch()
 	).count();
 }
 
-long get_milli_time() {
+long System_millitime() {
 	return std::chrono::duration_cast<std::chrono::milliseconds>(
 		std::chrono::system_clock::now().time_since_epoch()
 	).count();
 }
 
-long get_micro_time() {
+long System_microtime() {
 	return std::chrono::duration_cast<std::chrono::microseconds>(
 		std::chrono::system_clock::now().time_since_epoch()
 	).count();
 }
 
-long get_nano_time() {
+long System_nanotime() {
 	return std::chrono::duration_cast<std::chrono::nanoseconds>(
 		std::chrono::system_clock::now().time_since_epoch()
 	).count();
@@ -68,22 +68,6 @@ long get_nano_time() {
 Compiler::value System_operations(Compiler& c) {
 	jit_value_t jit_ops_ptr = jit_value_create_long_constant(c.F, jit_type_void_ptr, (long int) &c.vm->operations);
 	return {jit_insn_load_relative(c.F, jit_ops_ptr, 0, jit_type_uint), Type::LONG};
-}
-
-Compiler::value System_time(Compiler& c) {
-	return c.insn_call(Type::LONG, {}, (void*) get_sec_time);
-}
-
-Compiler::value System_millitime(Compiler& c) {
-	return c.insn_call(Type::LONG, {}, (void*) get_milli_time);
-}
-
-Compiler::value System_microtime(Compiler& c) {
-	return c.insn_call(Type::LONG, {}, (void*) get_micro_time);
-}
-
-Compiler::value System_nanotime(Compiler& c) {
-	return c.insn_call(Type::LONG, {}, (void*) get_nano_time);
 }
 
 Compiler::value System_version(Compiler& c) {
