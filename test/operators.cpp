b7f7ff397924710e5d444ad9f2b7e2467e5415a6
Pilow <pierre.laupretre@gmail.com>
2018-01-13 20:00:17 +0800

    [array][test] Those two tests are slightly modified

--- test/operators.cpp
+++ test/operators.cpp
@@ -65,7 +65,7 @@ void Test::test_operators() {
 	code("|null|").exception(ls::vm::Exception::NO_SUCH_OPERATOR);
 	code("null[2] = 5").semantic_error(ls::SemanticError::Type::VALUE_MUST_BE_A_CONTAINER, {"null"});
 	code("let a = [null, ''][0] a[2]").exception(ls::vm::Exception::NO_SUCH_OPERATOR);
-	code("let a = [null, ''][0] a[2] = 5").exception(ls::vm::Exception::NO_SUCH_OPERATOR);
+	code("let a = [null, ''][0] a[2] = 5").semantic_error(ls::SemanticError::NO_SUCH_OPERATOR, {});
 	code("null[2:5]").semantic_error(ls::SemanticError::Type::VALUE_MUST_BE_A_CONTAINER, {"null"});
 	code("null[2:5] = 4").semantic_error(ls::SemanticError::Type::VALUE_MUST_BE_A_CONTAINER, {"null"});
 	code("(5 + 2) += 4").semantic_error(ls::SemanticError::Type::VALUE_MUST_BE_A_LVALUE, {"5 + 2"});
@@ -73,7 +73,7 @@ void Test::test_operators() {
 	code("'hello'[2] = 5").semantic_error(ls::SemanticError::Type::NO_SUCH_OPERATOR, {});
 	code("'hello'[2:5] = 5").semantic_error(ls::SemanticError::Type::NO_SUCH_OPERATOR, {});
 	code("let a = [null, ''][0] a[2:5]").exception(ls::vm::Exception::NO_SUCH_OPERATOR);
-	code("var a = [null, ''][0] a[2:5] = 5").exception(ls::vm::Exception::NO_SUCH_OPERATOR);
+	code("var a = [null, ''][0] a[2:5] = 5").semantic_error(ls::SemanticError::NO_SUCH_OPERATOR, {});
 	code("var a = [null, ''][0] a.toto = 5").exception(ls::vm::Exception::NO_SUCH_OPERATOR);
 	code("12 in [12, ''][0]").exception(ls::vm::Exception::NO_SUCH_OPERATOR);
 	code("'hello' in [12, ''][0]").exception(ls::vm::Exception::NO_SUCH_OPERATOR);
