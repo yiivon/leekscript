66ff8228306d080204aae7b325c69b3313cbac37
Pilow <pierre.laupretre@gmail.com>
2018-12-12 17:01:14 +0100

    [type] Move type creation in Type class

--- src/type/Type.cpp
+++ src/type/Type.cpp
@@ -22,9 +22,13 @@ void Type::clear_placeholder_types() {
 
 unsigned int Type::placeholder_counter = 0;
 
-const Type Type::ANY(RawType::ANY);
-const Type Type::CONST_ANY(RawType::ANY, false, false, true);
-const Type Type::NULLL(RawType::NULLL, true);
+const std::shared_ptr<const Bool_type> RawType::BOOLEAN = std::make_shared<Bool_type>();
+const std::shared_ptr<const Integer_type> RawType::INTEGER = std::make_shared<Integer_type>();
+const std::shared_ptr<const Long_type> RawType::LONG = std::make_shared<Long_type>();
+const std::shared_ptr<const Real_type> RawType::REAL = std::make_shared<Real_type>();
+const std::shared_ptr<const Mpz_type> RawType::MPZ = std::make_shared<Mpz_type>();
+const std::shared_ptr<const Number_type> RawType::NUMBER = std::make_shared<Number_type>();
+
 const Type Type::BOOLEAN(RawType::BOOLEAN);
 const Type Type::CONST_BOOLEAN(RawType::BOOLEAN, false, false, true);
 const Type Type::NUMBER(RawType::NUMBER);
@@ -37,6 +41,16 @@ const Type Type::LONG(RawType::LONG);
 const Type Type::CONST_LONG(RawType::LONG, false, false, true);
 const Type Type::REAL(RawType::REAL);
 const Type Type::CONST_REAL(RawType::REAL, false, false, true);
+
+const std::shared_ptr<const Any_type> RawType::ANY = std::make_shared<Any_type>();
+const std::shared_ptr<const Null_type> RawType::NULLL = std::make_shared<Null_type>();
+const std::shared_ptr<const String_type> RawType::STRING = std::make_shared<String_type>();
+const std::shared_ptr<const Object_type> RawType::OBJECT = std::make_shared<Object_type>();
+const std::shared_ptr<const Class_type> RawType::CLASS = std::make_shared<Class_type>();
+
+const Type Type::ANY(RawType::ANY);
+const Type Type::CONST_ANY(RawType::ANY, false, false, true);
+const Type Type::NULLL(RawType::NULLL, true);
 const Type Type::STRING(RawType::STRING);
 const Type Type::CONST_STRING(RawType::STRING, false, false, true);
 const Type Type::STRING_TMP(RawType::STRING, false, true);
