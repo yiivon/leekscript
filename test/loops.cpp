5a2c342b8ccce6b75210b24c286cd7c26a66acbe
Pilow <pierre.laupretre@gmail.com>
2018-12-25 11:47:56 +0100

    [loops] Add test

--- test/loops.cpp
+++ test/loops.cpp
@@ -86,6 +86,7 @@ void Test::test_loops() {
 	code("var i = 0 while i < 10 do i++ end i").equals("10");
 	code("var i = 5 while (i-- > 0) { System.print(i) }").output("4\n3\n2\n1\n0\n");
 	code("while (true) { return 12 }").equals("12");
+	code("var n = 5 var a = [] while n-- { a += 1 }").equals("(void)");
 
 	/*
 	 * For loops
