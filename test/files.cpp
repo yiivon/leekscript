bd393d35a76418701123ec2fa20e07301b68d4a6
Pilow <pierre.laupretre@gmail.com>
2018-12-27 16:14:04 +0100

    [test] Re-enable project euler 8

--- test/files.cpp
+++ test/files.cpp
@@ -56,7 +56,7 @@ void Test::test_files() {
 	file("test/code/euler/pe005.leek").equals("232792560");
 	file("test/code/euler/pe006.leek").equals("25164150");
 	file("test/code/euler/pe007.leek").equals("104743");
-	DISABLED_file("test/code/euler/pe008.leek").equals("23514624000");
+	file("test/code/euler/pe008.leek").equals("23514624000");
 	file("test/code/euler/pe009.leek").equals("31875000");
 	file("test/code/euler/pe010.leek").equals("142913828922");
 	file("test/code/euler/pe011.leek").equals("70600674");
