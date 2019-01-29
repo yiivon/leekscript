4eff0f3aa77e686e474a2ab8825772763602d07b
Pilow <pierre.laupretre@gmail.com>
2019-01-29 08:01:43 +0100

    [number] Re-enable a test

--- test/number.cpp
+++ test/number.cpp
@@ -521,7 +521,7 @@ void Test::test_numbers() {
 	code("(-19.5).abs()").equals("19.5");
 	code("12.abs").equals("<function>");
 	code("12.abs").equals("<function>");
-	DISABLED_code("Number.abs([1, 'salut'][1])").exception(ls::vm::Exception::WRONG_ARGUMENT_TYPE);
+	code("Number.abs([1, 'salut'][1])").exception(ls::vm::Exception::WRONG_ARGUMENT_TYPE);
 
 	section("Number.exp()");
 	code("Number.exp(0)").equals("1");
