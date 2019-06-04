fa579c8383e79b81305de0a40756afcf1a262c17
Pilow <pierre.laupretre@gmail.com>
2019-06-04 23:13:07 +0200

    [test] Add todo about issue 243

--- test/array.cpp
+++ test/array.cpp
@@ -516,6 +516,7 @@ void Test::test_arrays() {
 	code("var a = [1] a.push('a') a").equals("[1, 'a']");
 	code("var a = [1] a.push('a')").equals("[1, 'a']");
 	code("var a = [] for i in [1..10] { a = a.push(i) } a").equals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+	// TODO issue #243
 	DISABLED_code("let p = (a, e) => a.push(e) var a = [] for i in [1..10] { a = p(a, i) } a").equals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
 
 	section("Array.pushAll()");
