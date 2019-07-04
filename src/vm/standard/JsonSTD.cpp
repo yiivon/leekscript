5cee3bf3c6bf99ca9c71a1dd9e76eb19dc76dbcc
Pilow <pierre.laupretre@gmail.com>
2019-07-04 22:58:57 +0200

    [json] Fix json encode return type

--- src/vm/standard/JsonSTD.cpp
+++ src/vm/standard/JsonSTD.cpp
@@ -20,22 +20,22 @@ JsonSTD::JsonSTD(VM* vm) : Module(vm, "Json") {
 
 Compiler::value JsonSTD::encode(Compiler& c, std::vector<Compiler::value> args, int) {
 	if (args[0].t->is_integer()) {
-		return c.insn_call(Type::string, args, "Number.int_to_string");
+		return c.insn_call(Type::tmp_string, args, "Number.int_to_string");
 	} else if (args[0].t->is_long()) {
-		return c.insn_call(Type::string, args, "Number.long_to_string");
+		return c.insn_call(Type::tmp_string, args, "Number.long_to_string");
 	} else if (args[0].t->is_real()) {
-		return c.insn_call(Type::string, args, "Number.real_to_string");
+		return c.insn_call(Type::tmp_string, args, "Number.real_to_string");
 	} else if (args[0].t->is_bool()) {
-		return c.insn_call(Type::string, args, "Boolean.to_string");
+		return c.insn_call(Type::tmp_string, args, "Boolean.to_string");
 	} else if (args[0].t->is_mpz_ptr()) {
-		auto s = c.insn_call(Type::string, args, "Number.mpz_to_string");
+		auto s = c.insn_call(Type::tmp_string, args, "Number.mpz_to_string");
 		if (args[0].t->temporary) {
 			c.insn_delete_mpz(args[0]);
 		}
 		return s;
 	}
 	// Default type : pointer
-	return c.insn_call(Type::string, args, "Value.json");
+	return c.insn_call(Type::tmp_string, args, "Value.json");
 }
 
 LSValue* JsonSTD::decode(LSString* string) {
