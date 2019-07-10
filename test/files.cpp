fff745de6b8b35aaff462886a734fffcbc7c7b2c
Pilow <pierre.laupretre@gmail.com>
2019-07-10 20:29:07 +0200

    [files][test] Re-enable gcd test

--- test/files.cpp
+++ test/files.cpp
@@ -7,7 +7,7 @@ void Test::test_files() {
 	section("General");
 	file("test/code/primes.leek").equals("78498");
 	DISABLED_file("test/code/primes_gmp.leek").equals("9591");
-	DISABLED_file("test/code/gcd.leek").equals("151");
+	file("test/code/gcd.leek").equals("151");
 	file("test/code/strings.leek").almost(52.0, 12.0);
 	DISABLED_file("test/code/reachable_cells.leek").equals("383");// TODO issue #243
 	DISABLED_file("test/code/break_and_continue.leek").equals("2504"); // TODO issue #243
